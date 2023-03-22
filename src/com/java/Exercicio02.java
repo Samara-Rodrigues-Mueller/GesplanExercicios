468eeb1a37baf2b137b51f4e40eb479c1f506c35
Samara Rodrigues Mueller <samara07@gmail.com>
2023-03-21 22:05:59 -0300

    resolução do exercício 02

--- src/com/java/Exercicio02.java
+++ src/com/java/Exercicio02.java
@@ -1,9 +1,32 @@
 package com.java;
 
+import java.util.Scanner;
+
 public class Exercicio02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+				
+		Scanner sc = new Scanner (System.in);
+		int number1;
+		
+		System.out.println("Informe um número: ");
+		number1 = sc.nextInt();
+		
+		if (number1<0)
+		{
+			System.out.println("O número é negativo.");
+		}
+		else if (number1>0)
+		{
+			System.out.println("O número é positivo.");
+		}
+		else
+		{
+			System.out.println("O número é zero.");
+			
+		}
+			
+		
 
 	}
 

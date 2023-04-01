9249bc35030ec0955728d3e30fa12295e0044f19
Samara Rodrigues Mueller <samara07@gmail.com>
2023-04-01 16:26:10 -0300

    Commit do java exercício 01

--- src/com/java/Exercicio01.java
+++ src/com/java/Exercicio01.java
@@ -8,19 +8,19 @@ public class Exercicio01 {
 
 		Scanner sc = new Scanner(System.in);{
 
-		int number1;
-		
-		System.out.println("Informe um número: ");
-		number1 = sc.nextInt();
-		
-		if (number1%2 == 0)
-		{
-			System.out.println("O número é par.");
-		}
-		else 
-		{
-			System.out.println("O número é ímpar.");
-		}
+			int number1;
+
+			System.out.println("Informe um número: ");
+			number1 = sc.nextInt();
+
+			if (number1%2 == 0)
+			{
+				System.out.println("O número é par.");
+			}
+			else 
+			{
+				System.out.println("O número é ímpar.");
+			}
 
 		}
 	}

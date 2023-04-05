8126b253a6a863be765d83d6c7212a55e532801a
Samara Rodrigues Mueller <samara07@gmail.com>
2023-04-04 21:15:32 -0300

    commit exercício 05

---
+++ src/com/java/Exercicio05.java
@@ -0,0 +1,36 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Exercicio05 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Entre com um número: ");
+		int num = scan.nextInt();
+
+		for (int i = 2; i <= num; i++) {
+
+			boolean primo = true;
+
+			for (int j = 2; j < i; j++) {
+
+				if (i % j == 0) {
+					primo = false;
+				}
+			}
+
+			if (primo) {
+				System.out.println(i);
+
+			}
+		}
+	}
+}
+
+
+
+
+
+

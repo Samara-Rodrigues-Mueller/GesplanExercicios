5d28be276c2407a5f3ea0dc8977c4f379b31a7c0
Samara Rodrigues Mueller <samara07@gmail.com>
2023-04-11 00:36:37 -0300

    Commit Exercício 8

--- src/com/java/Exercicio08.java
+++ src/com/java/Exercicio08.java
@@ -1,10 +1,20 @@
 package com.java;
 
-public class Exercicio08 {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Exercicio08{
 
+	static double conversaoCelsiusFahrenheit(double C) {
+		return 9*C/5+32;
 	}
-
+	public static void main(String [] args) {
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Informe a temperatura em Celsius: ");
+		double celsius = leitor.nextInt();
+		System.out.println("O número convertido em Fahrenheit é: ");
+		System.out.println(conversaoCelsiusFahrenheit(celsius));
+		
+				
+	}
 }
+
